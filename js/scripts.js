fe0a48cd70c752f328ca9d407dfae8652466801c
Chris Slattery <chrisslattery08@gmail.com>
2023-09-22 15:30:56 +0100

    Added Contact Section and Footer

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//Leaflet Map
+var map = L.map('map').setView([53.342686, -6.267118], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
